71c0ce8268d87c617869c208d9a06fbb85e7d423
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-21 22:16:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker (name)
     "Hello, my name is #{name}."
 end
 
-def batch_badge_creator (array)
+def batch_badge_creator (attendees)
   array.each do |i|
-    puts "Hello, my name is #{i}"
+    "Hello, my name is #{i}"
   end
 end
