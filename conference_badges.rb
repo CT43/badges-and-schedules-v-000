a5a0bd5c0a2544c1c5d217edb3b2f358ad5e0410
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-21 22:41:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,13 @@ def batch_badge_creator (attendees)
   end
   badges
 end
+
+def assign_rooms (attendees)
+  room_assignments = []
+  room_number = 1
+  attendees.each do |i|
+    room_assignments << "Hello, #{i}! You'll be assigned to room #{room_number}"
+    room_number += 1
+  end
+  room_assignments
+end
